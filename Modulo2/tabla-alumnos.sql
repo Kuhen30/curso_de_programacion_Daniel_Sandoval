307614ee3eefbfb2b2070501af86996797ece29b
Daniel Sandoval <daniel.ssoto05@gmail.com>
2025-08-08 14:25:49 -0400

    SQL
    Tarea clase

---
+++ Modulo2/tabla-alumnos.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Alumnos{
+    id INT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Apellido VARCHAR(100),
+    Cedula VARCHAR(100)
+};
+
+INSERT INTO Aalumnos (
+  id,
+  nombre, 
+  Apellido,
+  Cedula
+) 
+
+VALUES
+(29767741, 'Jinmy', 'Chacin', "12"),
+(30604870, 'Maria', 'Urdaneta', "2"),
+(30820000, 'Daniel', 'Sandoval',"3"),
+(31279773, 'Daviannys', 'Villalobos',"4"),
+(31913070, 'Humberto', 'Diaz',"22"),
+(32056687, 'German', 'Romero',"13"),
+(32410084, 'Julio', 'Paz',"32"),
+(32636138, 'Juan', 'Tami',"42"),
+(32698769, 'Jose', 'Morillo',"44"),
+(33319114, 'Gabriel', 'Suarez',"500"),
+(33607751, 'Roberti', 'Boscan',"400"),
+(33893333, 'Carlos', 'Moreno',"300"),
+(33966984, 'Jimn', 'Chacin',"200"),
+(34544638, 'Juan', 'Arrieta',"100");
